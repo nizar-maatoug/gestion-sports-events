f8b102d3e3dfa82491756eb7c9d0bdf2ea7e7b53
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-04-05 14:59:55 +0100

    Creation Modele v1

---
+++ app/Models/athlete.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class athlete extends Model
+{
+    use HasFactory;
+}
